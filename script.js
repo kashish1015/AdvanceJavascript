83cb3706e4235d0556a657e8a959fa64af03c7bb
Kashish Singh <kashish.23b0131066@abes.ac.in>
2025-04-02 21:11:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -15,8 +15,20 @@
 // const arg2 = "arguement 2";
 // tester(arg1, arg2);
 // console.log(tester);
-let x=prompt("Enter a number");
-let y=prompt("Enter a number");
-function addTwoNumbers(x,y) {
-    console.log(parseInt(x)+parseInt(y));
+
+function addTwoNumbers() {
+    let x = prompt("Enter a number:");
+    let y = prompt("Enter another number:");
+
+    let num1 = parseInt(x);
+    let num2 = parseInt(y);
+
+    let sum = num1 + num2;
+    console.log("The sum is: " + sum);
+    return sum;
 }
+
+addTwoNumbers();
+
+
+
