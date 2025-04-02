560f6388a593082bffba35e9e9bc1667497603bc
kashish1015 <singhkashish939@gmail.com>
2025-04-02 11:28:20 +0530

    func

---
+++ script.js
@@ -0,0 +1,22 @@
+// function sayHello() {
+//     let you = prompt("What's your name? ");
+//     console.log("Hello",you + "I");
+// }
+// sayHello();
+// let varContainingFunction = function() {
+//     let varInFunction = "I'm in a function. ";
+//     console.log("hi! there",varInFunction);
+// };
+// varContainingFunction();
+// function tester(para1,para2){
+//     return para1 + " " + para2;
+// }
+// const arg1 = "arguement 1";
+// const arg2 = "arguement 2";
+// tester(arg1, arg2);
+// console.log(tester);
+let x=prompt("Enter a number");
+let y=prompt("Enter a number");
+function addTwoNumbers(x,y) {
+    console.log(parseInt(x)+parseInt(y));
+}
