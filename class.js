f410b1ad791d82d4e23c93bf6c93e72e1fcaea22
kashish1015 <singhkashish939@gmail.com>
2025-04-18 12:59:29 +0530

    classes

--- class.js
+++ class.js
@@ -1,5 +1,7 @@
 //classes are a blyeprint or template for object creation
 //properties aur methods ek baar likhna is sufficient
+//class is just a declaration
+//class is  a collection of attributes and methods
 
 
 //classes and objects
@@ -8,7 +10,7 @@
 //     dogName:"JavaScript",
 //     weight:2.4,
 //     color:"brown",
-    
+
 // };
 
 // class ClassName{
@@ -41,6 +43,8 @@
 
 
 //Contructors=>invoked at the time of object creation
+//constructor to initialize object attribute values.
+//constructor automatically initialize ho jaata hai
 //class name with capital
 //there can be only one constructor in a program
 // class Person {
@@ -53,8 +57,143 @@
 // console.log("Hi",p.firstname);
 
 
-//METHODS
-//Functions on a class are called methods.When defining these methods,we dont use function keyword.we start directly with name
+//METHODS are user defined function
+//Functions on a class are called methods.When defining these methods,we dont use function keyword,we start directly with name
+// class Person {
+//     constructor(firstname,lastname){
+//         this.firstname=firstname;
+//         this.lastname=lastname;
+//     }
+//     greet(){
+//         console.log("hi there! I'm",this.firstname);
+//     }
+//     compliment(name,object){
+//         return "That's a wonderful" + object + ","+ name;
+
+//     }
+// }
+// let p= new Person("kashish","singh");
+// p.greet();
+// let compliment=p.compliment("Harry","hat");
+// //apne hi attributes bna kar krenge
+// console.log(compliment);
+
+//PROPERTIES are sometime called as fields,holds the data of the class.It is the initial case.This is the first step of abstraction
+// class Person{
+//     constructor(firstname,lastname){
+//     this.firstname=firstname;
+//     this.lastname=lastname;
+// }
+// }
+// let p=new Person("kashish","single");
+// console.log("hi",p.firstname);
+
+//private access specifier
+//#daalenge to make it private
+// class Person {
+//     #firstname;
+//     #lastname;
+//     constructor(firstname, lastname) {
+//         this.#firstname = firstname;
+//         this.#lastname = lastname;
+//     }
+// }
+// let p = new Person("kashish", "single");
+// console.log("hi", p.firstname);
+//undefined mtlb class ne koi value return nhi kar raha
+
+//validation
+// class Person {
+//     #firstname;
+//     #lastname;
+
+//     constructor(firstname, lastname) {
+//         if (firstname.startsWith("M")) {
+//             this.#firstname = firstname;
+//         } else {
+//             this.#firstname = "M" + firstname;
+//         }
+//         this.#lastname = lastname;
+//     }
+
+//     getData() {
+//         return `First Name: ${this.#firstname}, Last Name: ${this.#lastname}`;
+//     }
+// }
+
+// let p = new Person("kay", "Moon");
+// console.log(p.getData());
+
+
+
+//getters 
+// A getter is a method that gets the value of a property. It allows you to access a value like a regular property, but behind the scenes, it runs a function.
+//get always returns a value
+//setters can set or reinitialize the value
+// class Person {
+//     #firstname;
+//     #lastname;
+//     constructor(firstname, lastname) {
+//         this.#firstname = firstname;
+//         this.#lastname = lastname;
+//     }
+//     get firstname() {
+//         return this.#firstname;
+//     }
+
+//     set firstname(firstname){
+//         this.#firstname=firstname;
+//     }
+//     get lastname(){
+//         return this.#lastname;
+//     }
+//     set lastname(lastname){
+//         this.#lastname=lastname;
+//     }
+// }
+// let p= new Person("kashish","singh");
+// p.firstname="Ayush";
+// console.log(p.firstname);
+//direct access kr le rhe data ko hide kr le rhe encapsulation ho rha
+
+//INHERITANCE
+//reusability of code
+//inherits properties and methods of parent class
+//super keyword child se parent ko call krna hai toh
+// class Vehicle {
+//     constructor(color, currentSpeed, maxSpeed) {
+//         this.color = color;
+//         this.currentSpeed = currentSpeed;
+//         this.maxSpeed = maxSpeed;
+//     }
+//     move() {
+//         console.log("moving at", this.currentSpeed);
+//     }
+//     accelerate(amount) {
+//         this.currentSpeed += amount;
+//     }
+// }
+// class Motorcycle extends Vehicle {
+//     constructor(color, currentSpeed, maxSpeed, fuel) {
+//         super(color, currentSpeed, maxSpeed);
+//         this.fuel = fuel;
+//     }
+//     doWheelie() {
+//         console.log("Driving on one wheel");
+
+//     }
+
+// }
+// let motor=new Motorcycle("Black",0,250,"gasoline");
+// console.log(motor.color);
+// motor.accelerate(50);
+// motor.move();
+//we cannot access any Motorcycle specific properties or methods in our vehicle class
+
+//PROTOTYPES
+//that makes it possible to have objects.When nothing is specified when creating a class the objects inherits from objecy.prototype prototype.
+//CLassName.prototype
+//to add properties or methods to an object
 class Person {
     constructor(firstname,lastname){
         this.firstname=firstname;
@@ -63,13 +202,11 @@ class Person {
     greet(){
         console.log("hi there! I'm",this.firstname);
     }
-    compliment(name,object){
-        return "That's a wonderful" + object+ ","+name;
-
-    }
 }
-let p= new Person("kashish","singh");
-p.greet();
-let compliment=p.compliment("Harry","hat");
-//apne hi attributes bna kar krenge
-console.log(compliment);
+Person.prototype.introduce = function () {
+    console.log("Hi,I'm",this.firstname);
+};
+Person.prototype.favouriteColor = "green";
+let p = new Person("Kashish","Singh");
+console.log(p.favouriteColor);
+p.introduce();
